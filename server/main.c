4334abac166f0b147750b73a0d3cb06c3afc3c93
Jacob Champion <champion.p@gmail.com>
2016-04-25 14:41:39 -0700

    Add DUMP_INCLUDES to the usage info

--- server/main.c
+++ server/main.c
@@ -448,6 +448,8 @@ static void usage(process_rec *process)
     ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
                  "  -M                 : a synonym for -t -D DUMP_MODULES");
     ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
+                 "  -t -D DUMP_INCLUDES: show all included configuration files");
+    ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
                  "  -t                 : run syntax check for config files");
     ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
                  "  -T                 : start without DocumentRoot(s) check");
